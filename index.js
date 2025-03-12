e53b820c32d4a754426546ca558b2c8762555900
Man <nguyenman025@gmail.com>
2025-03-12 13:57:33 -0400

    add book functionality

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-class Shelf {
-    constructor(books=[]) {
-        // properties for each shelf instance
-        this.books = books;
-    }
-    addBook(book) {
-        // method to add a book to the shelf
-        this.books.push(book);
-    }
-    removeBook(book) {
-        // method to remove a book from the shelf
-        this.books = this.books.filter(element => element != book);
-    }
-}
-  
+const libraryContainer = document.querySelector('.library-container');
+// event listener for adding to library
+const addBookForm = document.querySelector('.book-add');
+const modal = document.querySelector('#book-modal');
+addBookForm.addEventListener('click', () => {
+    modal.showModal();
+});
+
 class Book {
     constructor(title, author, pages, readStatus) {
         // properties for each book instance
@@ -43,46 +36,114 @@ class Book {
     }
 }
 
-const myShelf = new Shelf([new Book('Battle Mountain', 'C.J. Box', 368, true), new Book('Harry Potter', 'J.K. Rowling', 350, false)]);
-
-const libraryContainer = document.querySelector('.library-container');
-
-// display books on the page
-myShelf.books.reverse().forEach(book => {
-    const bookElement = document.createElement('div');
-    bookElement.classList.add('book-card');
-    // properties
-    const title = document.createElement('h2');
-    title.textContent = book.title;
-    bookElement.appendChild(title);
-
-    const author = document.createElement('p');
-    author.textContent = `By ${book.author}`;
-    bookElement.appendChild(author);
-
-    const pages = document.createElement('p');
-    pages.textContent = `Pages: ${book.pages}`;
-    bookElement.appendChild(pages);
-
-    const actions = document.createElement('div');
-    actions.classList.add('book-actions');
-
-    const readStatus = document.createElement('button');
-    readStatus.type = 'button';
-    readStatus.textContent = 'Read';
-    readStatus.classList.add(`${book.readStatus? 'read' : 'not-read'}`);
-    readStatus.classList.remove(`${book.readStatus? 'not-read' : 'read'}`);
-    actions.appendChild(readStatus);
-    readStatus.addEventListener('click', () => {
-        book.toggleReadStatus();
+class Shelf {
+    constructor(books=[]) {
+        // properties for each shelf instance
+        this.books = books;
+    }
+    displayShelf() {
+        this.books.forEach(book => {
+            const bookElement = document.createElement('div');
+            bookElement.id = book.title.toLowerCase().split(' ').join('-') + book.pages;
+            bookElement.classList.add('book-card');
+            // properties
+            const title = document.createElement('h2');
+            title.textContent = book.title;
+            bookElement.appendChild(title);
+        
+            const author = document.createElement('p');
+            author.textContent = `By ${book.author}`;
+            bookElement.appendChild(author);
+        
+            const pages = document.createElement('p');
+            pages.textContent = `Pages: ${book.pages}`;
+            bookElement.appendChild(pages);
+        
+            const actions = document.createElement('div');
+            actions.classList.add('book-actions');
+        
+            const readStatus = document.createElement('button');
+            readStatus.type = 'button';
+            readStatus.textContent = 'Read';
+            readStatus.classList.add(`${book.readStatus? 'read' : 'not-read'}`);
+            readStatus.classList.remove(`${book.readStatus? 'not-read' : 'read'}`);
+            actions.appendChild(readStatus);
+            readStatus.addEventListener('click', () => {
+                book.toggleReadStatus();
+                readStatus.classList.add(`${book.readStatus? 'read' : 'not-read'}`);
+                readStatus.classList.remove(`${book.readStatus? 'not-read' : 'read'}`);
+            });
+        
+            const deletebtn = document.createElement('button');
+            deletebtn.type = 'button';
+            deletebtn.textContent = "Delete";
+            deletebtn.addEventListener('click', () => {
+                this.removeBook(book);
+            });
+            
+            actions.appendChild(deletebtn);
+            bookElement.appendChild(actions);
+            libraryContainer.insertBefore(bookElement, libraryContainer.lastElementChild);
+        });
+    }
+    addBook(book) {
+        // method to add a book to the shelf
+        this.books.push(book);
+        const bookElement = document.createElement('div');
+        bookElement.classList.add('book-card');
+        // properties
+        const title = document.createElement('h2');
+        title.textContent = book.title;
+        bookElement.appendChild(title);
+    
+        const author = document.createElement('p');
+        author.textContent = `By ${book.author}`;
+        bookElement.appendChild(author);
+    
+        const pages = document.createElement('p');
+        pages.textContent = `Pages: ${book.pages}`;
+        bookElement.appendChild(pages);
+    
+        const actions = document.createElement('div');
+        actions.classList.add('book-actions');
+    
+        const readStatus = document.createElement('button');
+        readStatus.type = 'button';
+        readStatus.textContent = 'Read';
         readStatus.classList.add(`${book.readStatus? 'read' : 'not-read'}`);
         readStatus.classList.remove(`${book.readStatus? 'not-read' : 'read'}`);
-    });
+        actions.appendChild(readStatus);
+        readStatus.addEventListener('click', () => {
+            book.toggleReadStatus();
+            readStatus.classList.add(`${book.readStatus? 'read' : 'not-read'}`);
+            readStatus.classList.remove(`${book.readStatus? 'not-read' : 'read'}`);
+        });
+    
+        const deletebtn = document.createElement('button');
+        deletebtn.type = 'button';
+        deletebtn.textContent = "Delete";
+        actions.appendChild(deletebtn);
+        bookElement.appendChild(actions);
+        libraryContainer.insertBefore(bookElement, libraryContainer.lastElementChild);
+    }
+    removeBook(book) {
+        // method to remove a book from the shelf
+        this.books = this.books.filter(element => element != book);
+        const bookElement = document.getElementById(book.title.toLowerCase().split(' ').join('-') + book.pages);
+        libraryContainer.removeChild(bookElement);
+    }
+}
+const myShelf = new Shelf([new Book('Battle Mountain', 'C.J. Box', 368, true), new Book('Harry Potter', 'J.K. Rowling', 350, false)]);
+myShelf.displayShelf();
+// display books on the page
 
-    const edit = document.createElement('button');
-    edit.type = 'button';
-    edit.textContent = "Edit"
-    actions.appendChild(edit);
-    bookElement.appendChild(actions);
-    libraryContainer.insertBefore(bookElement, libraryContainer.firstChild);
+modal.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const pages = parseInt(event.target.pages.value);
+    const readStatus = event.target['read-status'] === 'read';
+    const newBook = new Book(title, author, pages, readStatus);
+    myShelf.addBook(newBook);
+    modal.close();
 });
